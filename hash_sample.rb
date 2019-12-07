2582727a8a3a8035e7708837268d6f59f4255709
DoroYoshi <yoshimatsu44dy@gmail.com>
2019-12-07 14:58:28 +0900

    1 verificationメソッドを作成し、判定する部分を移行した 2 if文の条件を反転させて簡略化した

--- hash_sample.rb
+++ hash_sample.rb
@@ -5,32 +5,29 @@ class User
     @height = params[:height]
   end
 
-  def judge
-    puts "----------------------"
+  def display
+    puts "-" * 22
     puts "#{@name}さん"
-    if (@age <= 64) && (@height >= 130)
-      puts "年齢　#{@age}歳:年齢確認OK"
-      puts "身長　#{@height}cm:身長確認OK"
-      puts "\nご利用いただけます"
-    elsif (@age <= 64) && (@height < 130)
-      puts "年齢　#{@age}歳:年齢確認OK"
-      puts "身長　#{@height}cm:身長確認NG"
-      puts "\nご利用いただけません"
-    elsif (@age > 64) && (@height >= 130)
-      puts "年齢　#{@age}歳:年齢確認NG"
-      puts "身長　#{@height}cm:身長確認OK"
+    if (@age > 64) || (@height < 130)
+      verification
       puts "\nご利用いただけません"
     else
-      puts "年齢　#{@age}歳:年齢確認NG"
-      puts "身長　#{@height}cm:身長確認NG"
-      puts "ご利用いただけません"
+      verification
+      puts "\nご利用いただけます"
     end
+  end
+
+  def verification
+    age_veri = (@age <= 64) ? "OK" : "NG"
+    height_veri = (@height >= 130) ? "OK" : "NG"
+    puts "年齢　#{@age}歳:年齢確認#{age_veri}"
+    puts "身長　#{@height}cm:身長確認#{height_veri}"
   end
 end
 
 user = User.new(name: "田中", age: 30, height: 160)
-puts user.judge
+puts user.display
 user = User.new(name: "佐藤", age: 70, height: 150)
-puts user.judge
+puts user.display
 user = User.new(name: "鈴木", age: 10, height: 120)
-puts user.judge
+puts user.display
